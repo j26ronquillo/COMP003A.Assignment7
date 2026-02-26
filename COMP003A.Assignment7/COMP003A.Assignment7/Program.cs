dfc1e5e2eb7cbe51f6505b3555a5c0738788e37e
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-02-25 20:35:16 -0800

    created the try-catch statement and the if/elseif commands. must complete the different methods for each calculation

--- COMP003A.Assignment7/COMP003A.Assignment7/Program.cs
+++ COMP003A.Assignment7/COMP003A.Assignment7/Program.cs
@@ -1,24 +1,44 @@
-﻿namespace COMP003A.Assignment7
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Xml.Serialization;
+namespace COMP003A.Assignment7
 {
     internal class Program
     { // Student Score Analyzer 
-        static double CalculatedTotal();{
-        int list<int>
-        static void Main(string[] args)
-        {
-           List<int> list = new List<int>();
-           list.Add(92);
-           list.Add(74);
-           list.Add(83);
-           list.Add(79);
-           list.Add(99);
+		static void Main(string[] args)
+		{
+           List<int> scores = new List<int>();
+           scores.Add(92);
+           scores.Add(74);
+		   scores.Add(83);
+		   scores.Add(79);
+		   scores.Add(99);
 
             Console.WriteLine("1. Display Student Score");
             Console.WriteLine("2. Show Total");
             Console.WriteLine("3. Show Class Average");
-            Console.WriteLine("4. Exit Program");
-            
-             
+			Console.WriteLine("4. Exit Program");
+
+			bool program = true;
+            while (program)
+            {
+                Console.Write("Enter Valid Choice: ");
+            }
+            try
+            {
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice < 0 || choice > 4) ;
+                { 
+                    Console.WriteLine("ERROR: Invalid menu option. You must choose 1-4.");
+                }
+                else if (choice == 1) { DisplayScores(scores); }
+				else if (choice == 2) { DisplayTotal(scores); }
+				else if (choice == 3) { DisplayAverage(scores); }
+				else if (choice == 4) { program = false Console.WriteLine("Program ended."); }
+			}
+             catch (FormatException) { Console.WriteLine("Invalid input. Please enter a number."); }
         }
     }
 }
