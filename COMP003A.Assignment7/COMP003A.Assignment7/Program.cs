d03cbbd64bff5029f41a70bfc5329dcde6e0ac9e
joshalvinronquillo <j26ronquillo@gmail.com>
2026-02-25 18:27:38 -0800

    created the list and values with the display introduction menu.

---
+++ COMP003A.Assignment7/COMP003A.Assignment7/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace COMP003A.Assignment7
+{
+    internal class Program
+    { // Student Score Analyzer
+        static void Main(string[] args)
+        {
+           List<int> list = new List<int>();
+           list.Add(92);
+           list.Add(74);
+           list.Add(83);
+           list.Add(79);
+           list.Add(99);
+
+            Console.WriteLine("1. Display Student Score");
+            Console.WriteLine("2. Show Total");
+            Console.WriteLine("3. Show Class Average");
+            Console.WriteLine("4. Exit Program");
+            
+
+        }
+    }
+}
